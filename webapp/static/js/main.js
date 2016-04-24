54ce208158104056e10383ec0f2262ec09c2965c
Xinlu Huang <xinluh@stanford.edu>
2016-04-24 08:17:45 -0700

    MVP

--- webapp/static/js/main.js
+++ webapp/static/js/main.js
@@ -1,13 +1,48 @@
 $(document).ready(function() {
+	var providers = []
 	$('#output').hide()
-	$('#gobtn').click(function (){
-	    $.post('/search', {"zipcode": $('#zipcode').val(), "age": $('#age').val()}, 
+	var update_summary = function(zipcode, age, npi){
+	    $.post('/search', {"zipcode": zipcode, "age": age,"npi":npi}, 
 			   function(data){
-				   // $("#result").html(JSON.stringify(data));
 				   var res = JSON.parse(data);
+				   if (npi == "") {
+					   $('#plan-provider-item').hide()
+				   } else {
+					   $('#plan-provider-item').show()
+					   $('#plan-state-comp').html(res['state_comp'])
+					   $('#plan-state-comp').addClass((res['state_comp'].indexOf("pricier") > -1)?"bad":"good");
+				   }
 				   $('#plot').html(res['plot_div'])
 				   eval($(res['script']).text())
+				   $('#plan-num').html(res['num_plans'])
+				   $('#plan-national-comp').html(res['national_comp'])
+				   $('#plan-national-comp').addClass((res['national_comp'].indexOf("pricier") > -1)?"bad":"good");
+				   console.log(res['num_plans'])
 				   $('#output').show()
+				   console.log(res)
+				   
 			   });
+	}
+	$('#gobtn').click(function (){
+		update_summary($('#zipcode').val(), $('#age').val(),"")
 	});
+
+    $( "#provider" ).autocomplete({
+      source: function( request, response ) {
+        $.post('/search_providers', {"zipcode": $('#zipcode').val(), "age": $('#age').val(), "q":request.term }, 
+			   function(data){
+				   response(data.result);
+			   });
+      },
+      minLength: 3,
+      select: function( event, ui ) {
+		update_summary($('#zipcode').val(), $('#age').val(),ui.item.npi)
+        return true;
+      }
+	  })
+	  .autocomplete( "instance" )._renderItem = function( ul, item ) {
+      return $( "<li class='autocomplete'>" )
+        .append( "<a>" + item.label + "<br> <span class=desc>" + item.desc + "</span></a>" )
+        .appendTo( ul );
+    };;
 })
--- webapp/views.py
+++ webapp/views.py
@@ -2,10 +2,17 @@ from webapp import app
 from flask import render_template, request, jsonify,Response, send_from_directory
 import json
 from bokeh.plotting import figure
+from bokeh.charts import Bar
 from bokeh.embed import components 
 from bokeh import mpl
 import matplotlib.pyplot as plt
 import numpy as np
+from time import time
+import pandas as pd
+from datetime import datetime
+from bokeh.sampledata.autompg import autompg as df
+
+#print df
 
 @app.route('/')
 @app.route('/index')
@@ -18,23 +25,96 @@ def index():
 def search():
     age = request.form['age']
     zipcode = request.form['zipcode']
-    print age, zipcode
-    script,div = test_plot()
-    return json.dumps({'num_plans':35, 'script': script, 'plot_div': div})
+    npi = request.form['npi']
 
-def test_plot():
-    x = np.linspace(-2 * np.pi, 2 * np.pi, 100)
-    y = np.sin(x)
-    z = np.cos(x)
-     
-    plt.plot(x, y, "r-", marker='o')
-     
-    # dashed lines work
-    plt.plot(x, z, "g-x", linestyle="-.")
-        # plt.scatter( [1, 2, 3, 4, 5],[6, 7, 2, 4, 5])
-        # plt.title('testing')
+    print age, zipcode, npi
+    
+    return json.dumps(summarize_plans(zip2state(zipcode), age, npi))
 
-    p = mpl.to_bokeh()
+def age2nationalAverage(age):
+    age2avg = pd.read_csv('webapp/data/age2avg.csv', dtype=str)
+    avg = age2avg[age2avg['Age']==str(age)].IndividualRate.values[0]
+    return avg
+
+@app.route('/search_providers', methods=['POST'])
+def search_provider():
+    age = request.form['age']
+    zipcode = request.form['zipcode']
+    query = request.form['q'].lower()
+    # providers = pd.read_pickle('webapp/data/provider_info.pkl')
+    providers = pd.read_hdf('webapp/data/providers.h5','providers')
+    providers = providers[(providers.state == zip2state(zipcode)) & providers['last'].str.lower().str.startswith(query)]
+    jsons = providers.apply(lambda x: {'npi':x['npi'],
+                               'value': x['first'] +' ' + x['last'],
+                               'label':x['last']+', '+x['first'],
+                               'desc': ', '.join(x[['address','city','state','zip']])
+                              },axis=1).values
+    return jsonify(result=list(jsons))
+
+def zip2state(zipcode):
+    zippd = pd.read_csv('webapp/data/zip2state.csv', dtype=str)
+    state = zippd[zippd.Zipcode==str(zipcode)].State.values[0]
+    return state
+
+def summarize_plans(state, age, npi):
+    todaysdate = str(datetime.now())
+    age = str(age)
+    if age > '65':
+      age = '65'
+    elif age < '20':
+      age = '20'
+    # filter based on what plans are current (not expired) and age
+    filteredplans = pd.read_hdf('webapp/data/plan-rates.h5', state, where=['(Age==age) & (RateExpirationDate > todaysdate) & (RateEffectiveDate < todaysdate)'], columns = ['IndividualRate', 'MetalLevel', 'Age'])
+      
+    # plot it
+    #filteredplans.groupby('MetalLevel').IndividualRate.mean().plot(kind='bar')
+    #statebardf = filteredplans.groupby('MetalLevel').IndividualRate.mean()
+    statebardf = filteredplans.groupby('MetalLevel', as_index=False).mean()
+    
+    #p = mpl.to_bokeh()
+    if npi == '':
+      p = Bar(filteredplans.groupby('MetalLevel').IndividualRate.mean(), values='IndividualRate',
+              xlabel="", ylabel="Montly Premium ($)")
+    else:
+      print 'input npi is ' + npi
+      # read hdf5 of provider-plan pairings
+      provnplanpd = pd.read_hdf('webapp/data/plan_providers.h5', state)
+      # make a list of planids for the input npi
+      planlst = provnplanpd[provnplanpd.npi == npi].plan_id.values
+      print planlst
+      # read in hdf5 of plan info
+      planinfo = pd.read_hdf('webapp/data/plan-rates.h5', state, where=['(Age==age) & (RateExpirationDate > todaysdate) & (RateEffectiveDate < todaysdate)'])
+
+      # for each planid, get the first entry and concatenate them together.
+      filteredplans = planinfo[planinfo.PlanId==planlst[0]].groupby('PlanId', as_index=False).first()
+      filteredplans.head()
+      #pd.concat([filteredplans, filteredplans])
+
+      for ii in range(1, len(planlst)):
+          filteredplans = pd.concat([filteredplans, planinfo[planinfo.PlanId==planlst[ii]].groupby('PlanId', as_index=False).first()])
+
+    
+      provbardf = filteredplans.groupby('MetalLevel', as_index=False).mean()
+      provbardf['average'] = ['provider rates']*len(provbardf.MetalLevel.values)
+      statebardf['average'] = ['state average']*len(provbardf.MetalLevel.values)
+      #provbardf['average'] = [1, 1]
+      #statebardf['average'] = [2, 2]
+      #plotdf = pd.merge(statebardf, provbardf, on='MetalLevel')
+      plotdf = pd.concat([provbardf, statebardf]) 
+      #plotdf['state average'] = statebardf['IndividualRate']
+      #plotdf['provider rates'] = provbardf['IndividualRate']
+      print plotdf  
+      p = Bar(plotdf, label='MetalLevel', values='IndividualRate', group='average', legend='top_right',
+              xlabel="", ylabel="Montly Premium ($)")
     p.logo = None
+    p.plot_height=400
     p.toolbar_location = None
-    return components(p)
+    script,div =  components(p)
+
+    return {'num_plans':len(filteredplans), 'script': script, 'plot_div': div,
+            'national_comp': format_price_comp(age2nationalAverage(age)),
+            'state_comp': format_price_comp(filteredplans.IndividualRate.mean())}
+
+def format_price_comp(price):
+    price = int(float(price))
+    return ('$%d '% price) + ('cheaper' if price < 0 else 'pricier')
